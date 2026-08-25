510f1c2bcfab62d9179820e4d2a236fbd6ba2dff
keemdoesvideo <keemdoesvideo@gmail.com>
2026-08-25 01:32:23 +0100

    Serve isolated TurnupTaco ambience

--- server/app.py
+++ server/app.py
@@ -57,6 +57,7 @@ ALLOWED_ASSETS: dict[str, str] = {
     "fetcher-kaywordley.js": "application/javascript; charset=utf-8",
     "fetcher-jackigoe.js": "application/javascript; charset=utf-8",
     "fetcher-deenapie.js": "application/javascript; charset=utf-8",
+    "fetcher-turnuptaco.js": "application/javascript; charset=utf-8",
     "fetcher-favicon.svg": "image/svg+xml",
     "apple-touch-icon.png": "image/png",
     "fetcher-social-card.png": "image/png",
@@ -391,6 +392,7 @@ _LAUNCH_HEAD = """
 <script defer src="/fetcher-kaywordley.js"></script>
 <script defer src="/fetcher-jackigoe.js"></script>
 <script defer src="/fetcher-deenapie.js"></script>
+<script defer src="/fetcher-turnuptaco.js"></script>
 """.strip()
 
 
